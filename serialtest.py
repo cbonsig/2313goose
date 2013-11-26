e86a1e6770200afdea71c44847486df8867d2458
Craig Bonsignore <cbonsignore@Craigs-MacBook-Pro.local>
2013-11-25 20:22:18 -0800

    got serialtest.py working

--- serialtest.py
+++ serialtest.py
@@ -7,19 +7,13 @@ import serial # requires pySerial to be installed
 def getch(serialObject) :
 		r=''
 		while r=='': 
-			# print "before tty.read"
-			# self.tty.read(1)
-			serialObject.read()                  # getting stuck here! why?? never reads characters
-			print "past read()"
-			# print "after tty.read"
-			# print r
+			serialObject.read(1)  
 			if not r :
-				print "not r" 
 				time.sleep(0.020)
 		return r
 
 
-tty = serial.Serial('/dev/tty.usbmodem1d1131', 9600, timeout=0)
+tty = serial.Serial('/dev/tty.usbmodem1d1131', 9600)
 
 print tty.name
 
@@ -27,15 +21,8 @@ time.sleep(2.0)
 
 tty.flush()
 
-# tty.write(' H \n')
+tty.write(" H ")
 
-tty.write(' ')
-print tty.read()
-
-tty.write('H')
-print tty.read()
-
-tty.write(' ')
 print tty.readline()
 
 print "Done."
